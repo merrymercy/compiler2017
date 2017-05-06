4af2fcfebdc2ea8ebbe84888990e24441aa58703
MerryMercy <zhenglianmin96@163.com>
2017-05-07 04:44:08 +0800

    update codegen.bash

--- codegen.bash
+++ codegen.bash
@@ -1,2 +1,6 @@
-semantic.bash
+set -e
+cd "$(dirname "$0")"
+export CCHK="java -classpath ./lib/antlr-4.6-complete.jar:./bin com.mercy.Main"
+cat > testcase/test.c   # save everything in stdin to program.txt
+$CCHK
 cat out.asm
